653d600804b893ed4691770ef96503eb66705784
Ido Frenkel <ido.frenkel@post.runi.ac.il>
2024-11-08 08:22:10 +0200

    FVcalc.java implemented

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,22 @@
 // Computes the future value of a saving investment.
+// Example Input: % java FVCalc 100 10 2
+// Example Output: After 2 years, $100 saved at 10.0% will yield $121
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		// camelCase
+		// snake_case
+		// PascalCase
+		// Replace this comment with your code 
+		String startMoney = args[0];
+		String interestRate = args[1];
+		String years = args[2];
+		Double interestRateDouble = Double.parseDouble(interestRate) / 100;
+		Double startMoneyDouble = Double.parseDouble(startMoney);
+		Double yearsDouble = Double.parseDouble(years);
+		double futureValue = startMoneyDouble * Math.pow(1 + interestRateDouble, yearsDouble);
+		System.out.println("After " + years + " years, $" + startMoney +
+		                   " saved at " + interestRate + "% will yield $" + (int) futureValue);
+
+
 	}
 }
