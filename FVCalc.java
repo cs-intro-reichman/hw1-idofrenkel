6d96211b2279eb55d6954695f9e1b1b14941a584
Ido Frenkel <ido.frenkel@post.runi.ac.il>
2024-11-08 08:48:27 +0200

    FVCalc.java fix

--- FVCalc.java
+++ FVCalc.java
@@ -10,12 +10,12 @@ public class FVCalc {
 		String startMoney = args[0];
 		String interestRate = args[1];
 		String years = args[2];
-		Double interestRateDouble = Double.parseDouble(interestRate) / 100;
+		Double interestRateDouble = Double.parseDouble(interestRate);
 		Double startMoneyDouble = Double.parseDouble(startMoney);
 		Double yearsDouble = Double.parseDouble(years);
-		double futureValue = startMoneyDouble * Math.pow(1 + interestRateDouble, yearsDouble);
-		System.out.println("After " + years + " years, $" + startMoney +
-		                   " saved at " + interestRate + "% will yield $" + (int) futureValue);
+		double futureValue = startMoneyDouble * Math.pow(1 + interestRateDouble / 100, yearsDouble);
+		System.out.println("After " + years + " years, a $" + startMoney +
+		                   " saved at " + interestRateDouble + "% will yield $" + (int) futureValue);
 
 
 	}
